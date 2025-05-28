bc17ff8ad7d2a042b2637d8eb56af98fcaa4e5cd
Artem Rybintsev <streetball1988@gmail.com>
2025-05-28 16:38:43 +0300

    Изменил слова на символы

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,6 +2,6 @@
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 # Буби, черви, крести, пики.
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ['♦', '♠', '♥ ', '♣']
 
 puts "#{values.sample} of #{suits.sample}"
